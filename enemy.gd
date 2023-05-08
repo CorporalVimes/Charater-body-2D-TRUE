6dd3e6a8f732000d3a7187216d7485923e36bd70
CorporalVimes <132496027+CorporalVimes@users.noreply.github.com>
2023-05-08 12:15:38 -0400

    bounvh
    emeny now bonce

--- enemy.gd
+++ enemy.gd
@@ -7,22 +7,27 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = -1
 var speed = 200
 var score = 500
+var _was_on_wall = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("walk")
-	self.set_physics_process(false)
-
-func _start_game():
 	self.set_physics_process(true)
 
+
 func _physics_process(delta):
+	
 		#now with new GRAVITY
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
 	velocity.x += speed * delta * direction
+	if is_on_wall() and not _was_on_wall:
+		direction *= -1
+		_was_on_wall = true
+	else:
+		_was_on_wall = false
 	move_and_slide()
 
 func _on_attack_body_entered(body):
@@ -46,6 +51,7 @@ func _on_hit_area_entered(area):
 	self.set_deferred("freeze",true)
 	$CollisionShape2D.set_deferred("disabled", true)
 	$hit/CollisionShape2D.set_deferred("disabled", true)
+	self.call_deferred("set_physics_process", false)
 	$AnimatedSprite2D.play("death")
 	await $mob_Timer.timeout
 	queue_free()
